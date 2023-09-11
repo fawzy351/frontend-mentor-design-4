a1ff5cd4314b5b0306ef588aa4ee95df9a842301
fawzyebrahim351 <fawzyebrahim777@gmail.com>
2023-09-11 06:53:12 +0200

    html and css design from frontend mentor

---
+++ javascript/alaasar.js
@@ -0,0 +1,6 @@
+// show and hide mega menu 
+let menu = document.getElementById('menu');
+let others = document.getElementById('others');
+function showMegaMenu(){
+    menu.classList.toggle('show-menu');
+}
